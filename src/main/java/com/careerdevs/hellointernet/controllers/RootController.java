e60200de08cf03919278809571c0992e89eb35c2
Keith Benjamin <kjb528i@yahoo.com>
2022-03-03 18:21:48 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/hellointernet/controllers/RootController.java
@@ -0,0 +1,20 @@
+package com.careerdevs.hellointernet.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    private String rootRoute(){
+        return "You requested the root route";
+    }
+
+    @GetMapping("/random")
+    private int randomNum(){
+        return (int) (Math.floor(Math.random() * (100 - 1 + 1)) + 1);
+    }
+
+}
