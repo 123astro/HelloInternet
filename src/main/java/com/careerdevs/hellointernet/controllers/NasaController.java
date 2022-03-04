3bd82b40bd27c6f5dbd8e07cc9cd29b20cc71c1a
Keith Benjamin <kjb528i@yahoo.com>
2022-03-03 21:00:47 -0500

    added nasa mvp

--- src/main/java/com/careerdevs/hellointernet/controllers/NasaController.java
+++ src/main/java/com/careerdevs/hellointernet/controllers/NasaController.java
@@ -2,6 +2,7 @@ package com.careerdevs.hellointernet.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,10 +12,17 @@ import org.springframework.web.client.RestTemplate;
 public class NasaController {
 
     private String myNasaKey = "aBpzdFMKtVnmgmtc3GlQYfkocCVNsCxZeBfywHM2";
-    private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
+    public static String picDate = "2022-03-1";
+    private String nasaApodEndpoint =
+            "https://api.nasa.gov/planetary/apod?date="+ picDate +"&api_key=" + myNasaKey;
 
     @GetMapping("/apod")
     public Object apodHandler(RestTemplate restTemplate){
         return restTemplate.getForObject(nasaApodEndpoint, Object.class);
     }
+
+//    @GetMapping("/apod/{picDate}")
+//    public Object apodHandler(RestTemplate restTemplate, @PathVariable("picDate") picDate) {
+//        return restTemplate.getForObject(nasaApodEndpoint, Object.class);
+//    }
 }
