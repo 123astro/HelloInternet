aec56fee1ddb331603625d449075172ebd4726c4
Keith Benjamin <kjb528i@yahoo.com>
2022-03-07 21:25:26 -0500

    added security apikey to application.properties

--- src/main/java/com/careerdevs/hellointernet/controllers/NasaController.java
+++ src/main/java/com/careerdevs/hellointernet/controllers/NasaController.java
@@ -1,6 +1,8 @@
 package com.careerdevs.hellointernet.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +13,21 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/nasa") // endpoint
 public class NasaController {
 
-    private String myNasaKey = "aBpzdFMKtVnmgmtc3GlQYfkocCVNsCxZeBfywHM2";
-    private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
+    @Autowired
+    private Environment env;
+
+    private String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=";
 
     @GetMapping("/apod")
-    public Object apodHandler(RestTemplate restTemplate){
-        return restTemplate.getForObject(nasaApodEndpoint, Object.class);
+    public Object apodHandler(RestTemplate restTemplate) {
+        String apikey = env.getProperty("NASA_API_KEY");
+        return restTemplate.getForObject(nasaApodEndpoint + apikey, Object.class);
     }
 
     @GetMapping("/apod/{picDate}")
-    public Object apodHand(RestTemplate restTemplate, @PathVariable("picDate") String picDate ) {
-        return restTemplate.getForObject( "https://api.nasa.gov/planetary/apod?date="+ picDate +"&api_key=" + myNasaKey, Object.class);
+    public Object apodHand(RestTemplate restTemplate, @PathVariable("picDate") String picDate) {
+        String apikey = env.getProperty("NASA_API_KEY");
+        return restTemplate.getForObject("https://api.nasa.gov/planetary/apod?date=" + picDate + "&api_key=" + apikey,
+                Object.class);
     }
 }
